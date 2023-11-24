c400e39c4997bcf7ae910eca55edadafe6cf15e8
EputraP <EputraP@gmail.com>
2023-11-24 16:55:32 +0700

    20231124_sore

--- src/component/home/component/carousel/carousel.tsx
+++ src/component/home/component/carousel/carousel.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PauseIcon from "../../../../assets/pause_icon.png";
 import PlayIcon from "../../../../assets/play_icon.png";
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
-
+import NextIcon from "../../../../assets/next-icon.png";
+import BackIcon from "../../../../assets/back-icon.png";
 import "./carousel.scss";
 
 interface Props {
@@ -11,6 +11,7 @@ interface Props {
 
 const carousel = ({ data }: Props) => {
   const [slide, setSlide] = useState(0);
+  const [autoSlideVal, setAutoSlideVal] = useState<boolean>(false);
 
   const nextSlide = () => {
     setSlide(slide === data.length - 1 ? 0 : slide + 1);
@@ -20,48 +21,91 @@ const carousel = ({ data }: Props) => {
     setSlide(slide === 0 ? data.length - 1 : slide - 1);
   };
 
+  useEffect(() => {
+    if (autoSlideVal) {
+      const timer = setInterval(() => {
+        setSlide((prevSlide) => (prevSlide + 1) % data.length);
+      }, 5000);
+      return () => clearInterval(timer);
+    } else {
+      setSlide(0);
+    }
+  }, [autoSlideVal]);
+
   return (
     <div className="carousel">
       <div className="carousel__content">
         <div className="carousel__content__image">
-          {data.map((item: any, idx: any) => {
-            return (
-              <img
-                src={item.src}
-                alt={item.alt}
-                key={idx}
-                className={slide === idx ? "slide" : "slide slide-hidden"}
-              />
-            );
-          })}
+          <div
+            className="img-list-wrapper"
+            style={{ transform: `translate(-${slide * 100}%)` }}
+          >
+            {data.map((item: any, idx: any) => {
+              return (
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  key={idx}
+                  // className={slide === idx ? "slide" : "slide slide-hidden"}
+                  className={"slide"}
+                />
+              );
+            })}
+          </div>
         </div>
         <div className="carousel__content__text">
-          <div className="carousel__content__text__top" />
-          <div className="carousel__content__text__bottom">
-            <h4>Cybersecurity in orbit</h4>
-            <p>
-              As outer space gets more crowded and contested, QuSecure is
-              protecting communications and data transmission with the agility
-              required for the world of post-quantum computing.
-            </p>
-            <a>Read more</a>
+          <div
+            className="txt-list-wrapper"
+            style={{ transform: `translate(-${slide * 100}%)` }}
+          >
+            {data.map((item: any, idx: any) => {
+              return (
+                <div key={idx} className="carousel__content__text__bottom">
+                  <div
+                    style={{ height: "calc(100% - 320px)" }}
+                    className="top"
+                  ></div>
+                  <div
+                    style={{
+                      width: "100%",
+                      height: "400px",
+                      boxSizing: "border-box",
+                      padding: "50px",
+                    }}
+                    className="bottom"
+                  >
+                    <h4>{item.title}</h4>
+                    <p>{item.desc}</p>
+
+                    <a
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        // justifyContent: "center",
+                      }}
+                    >
+                      <label>Read More</label>
+                      <div className="click-button">{">"}</div>
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
       <div className="carousel__navigation">
         <div className="carousel__navigation__arrow">
-          <div>
-            <img className="play-background" src={PlayIcon} />
-            <img src={PauseIcon} />
+          <div onClick={() => setAutoSlideVal(!autoSlideVal)}>
+            {!autoSlideVal && (
+              <img className="play-background" src={PlayIcon} />
+            )}
+            {autoSlideVal && <img src={PauseIcon} />}
           </div>
-          {/* <BsArrowLeftCircleFill
-            onClick={prevSlide}
-            className="arrow arrow-left"
-          />
-          <BsArrowRightCircleFill
-            onClick={nextSlide}
-            className="arrow arrow-right"
-          /> */}
+          <div className="carousel__navigation__arrow__navigation-arrow">
+            <img onClick={prevSlide} src={BackIcon} />
+            <img onClick={nextSlide} src={NextIcon} />
+          </div>
         </div>
         <div className="carousel__navigation__dot">
           <span className="indicators">
--- src/component/home/home.tsx
+++ src/component/home/home.tsx
@@ -3,6 +3,9 @@ import TextMoveOnMouseMove from "./component/text-move-onMouseMove/text-move-onM
 import CardEvent from "./component/card-event/card-event";
 import Carousel from "./component/carousel/carousel";
 import DegValue from "./component/value-360/deg-value";
+import AccentureWoma from "../../assets/Accenutre-Woman-Smiling-With-Other-Woman-768x432_rad-3x2 (1).png";
+import Remaining from "../../assets/reimagining-saudi-arabia-economy-glanceskim-640x452_rad-3x2 (1).png";
+import MorningEat from "../../assets/Accenture-Father-Daughter-Breakfast-768x768_rad-3x2 (1).png";
 
 const home = () => {
   return (
@@ -13,16 +16,22 @@ const home = () => {
       <Carousel
         data={[
           {
-            src: "https://picsum.photos/seed/img1/600/400",
+            src: AccentureWoma,
             alt: "Image 1 for carousel",
+            title: "Creating a sense of belonging",
+            desc: "A global retailer and Accenture co-created a multiyear inclusion and diversity strategy to facilitate a greater sense of belonging for their people.",
           },
           {
-            src: "https://picsum.photos/seed/img2/600/400",
+            src: Remaining,
             alt: "Image 2 for carousel",
+            title: "Mondelēz International’s Data and AI Transformation",
+            desc: "The global snacking giant is laying a tasty foundation for reinvention and growth. Accenture is helping Mondelēz International to embrace the power of data and AI, build a strong digital core and implement interoperable cloud-enabled technology.",
           },
           {
-            src: "https://picsum.photos/seed/img3/600/400",
+            src: MorningEat,
             alt: "Image 3 for carousel",
+            title: "Reimagining Saudi Arabia’s economy",
+            desc: "In just five years, the Saudi Data and Artificial Intelligence Authority, in partnership with Accenture, has built a strong foundation for a globally competitive, data- and AI-driven economy.",
           },
         ]}
       />
